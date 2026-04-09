105b0633c89778b0804b216286d4c7e976f4fcec
dylandhw <dylanwilkins2308@gmail.com>
2026-04-08 20:39:49 -0400

    function to check if webcam is active

--- main.go
+++ main.go
@@ -1,15 +1,32 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
+	"os/exec"
 	"os/signal"
+	"strings"
 	"syscall"
 )
 
+func isWebcamActive() bool {
+	cmd := exec.Command("fuser", "/dev/video0")
+	output, err := cmd.CombinedOutput()
+
+	if err != nil {
+		fmt.Println("error: ", err)
+	}
+
+	outStr := strings.TrimSpace(string(output))
+	if outStr != "" {
+		return true
+	}
+	return false
+}
+
 func main() {
 	// logic
-
 	log.Println("started daemon")
 
 	quit := make(chan os.Signal, 1)
